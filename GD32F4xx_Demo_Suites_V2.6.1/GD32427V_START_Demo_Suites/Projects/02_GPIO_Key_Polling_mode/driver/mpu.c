9ff8d7e8b22f7682efd18bb5e40c29eb1b4dd17a
andey <qihengchuan8888@163.com>
2022-12-25 16:45:22 +0800

    add fmc control
    Change-Id: I9a5c4d59ca080f968b309b993dcef6ee4043a241

---
+++ GD32F4xx_Demo_Suites_V2.6.1/GD32427V_START_Demo_Suites/Projects/02_GPIO_Key_Polling_mode/driver/mpu.c
@@ -0,0 +1,6 @@
+#include "mpu_reg.h"
+#include "gd32f4xx.h"
+#include <stdio.h>
+#include "littleshell.h"
+#include "utilities.h"
+
---
+++ GD32F4xx_Demo_Suites_V2.6.1/GD32427V_START_Demo_Suites/Projects/02_GPIO_Key_Polling_mode/driver/mpu_reg.h
@@ -0,0 +1,9 @@
+#ifndef __MPU_REG_H__
+#define __MPU_REG_H__
+
+#define MPU_BASE      0xE000ED90
+#define MPU_TYPE_ADDR 0xE000ED90
+#define MPU_CTRL_ADDR 0xE000ED94
+#define MPU_RNR_ADDR  0xE000ED98
+
+#endif /* end of __MPU_REG_H__ */
