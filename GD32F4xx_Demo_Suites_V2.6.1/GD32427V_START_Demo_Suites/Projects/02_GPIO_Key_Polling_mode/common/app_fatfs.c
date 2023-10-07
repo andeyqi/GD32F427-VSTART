865a405e4273f80862c2f47884e82c291f7724fc
andeyqi <qihengchuan8888@163.com>
2023-10-07 21:09:28 +0800

    add fatfs test function

--- GD32F4xx_Demo_Suites_V2.6.1/GD32427V_START_Demo_Suites/Projects/02_GPIO_Key_Polling_mode/common/app_fatfs.c
+++ GD32F4xx_Demo_Suites_V2.6.1/GD32427V_START_Demo_Suites/Projects/02_GPIO_Key_Polling_mode/common/app_fatfs.c
@@ -201,7 +201,7 @@ unsigned int fatfs_test(char argc,char ** argv)
    int32_t ProcessStatus = 0;
 
   MX_FATFS_Init();
-  
+
   while (1)
   {
     ProcessStatus = MX_FATFS_Process();
@@ -221,4 +221,138 @@ unsigned int fatfs_test(char argc,char ** argv)
   }
 }
 LTSH_FUNCTION_EXPORT(fatfs_test,"fatfs test");
+
+
+FRESULT scan_files (
+    char* path        /* Start node to be scanned (***also used as work area***) */
+)
+{
+    FRESULT res;
+    DIR dir;
+    UINT i;
+    static FILINFO fno;
+
+
+    res = f_opendir(&dir, path);                       /* Open the directory */
+    if (res == FR_OK) {
+        for (;;) {
+            res = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
+            if (fno.fattrib & AM_DIR) {                    /* It is a directory */
+                i = strlen(path);
+                sprintf(&path[i], "/%s", fno.fname);
+                res = scan_files(path);                    /* Enter the directory */
+                if (res != FR_OK) break;
+                path[i] = 0;
+            } else {                                       /* It is a file. */
+                printf("%s/%s\r\n", path, fno.fname);
+            }
+        }
+        f_closedir(&dir);
+    }
+
+    return res;
+}
+
+unsigned int ls(char argc,char ** argv)
+{
+    char buff[20];
+
+    strcpy(buff, "/");
+    scan_files(buff);
+
+    return 0;
+}
+
+LTSH_FUNCTION_EXPORT(ls,"fatfs ls dir.");
+
+unsigned int fatfs(char argc,char ** argv)
+{
+  int cmd = 0;
+  FRESULT res; /* FatFs function common result code */
+  uint32_t byteswritten, bytesread; /* File write/read counts */
+  uint8_t wtext[] = "This is STM32 working with FatFs and SRAM diskio driver"; /* File write buffer */
+  uint8_t rtext[100]; /* File read buffer */
+  cmd = atoi(argv[1]);
+
+  switch(cmd)
+  {
+  case 0:
+    /*## FatFS: Link the disk I/O driver(s)  ###########################*/
+    SRAMDISKFatFS.fs_type = FS_FAT16;
+    if (FATFS_LinkDriver(&SRAMDISK_Driver, SRAMDISKPath) != 0)
+    {
+      printf("fatfs link driver error \r\n");
+    }
+    else
+    {
+      printf("fatfs link driver path = %s\r\n",SRAMDISKPath);
+    }
+    break;
+  case 1:
+    if (f_mkfs(SRAMDISKPath, FM_ANY, 0, workBuffer, sizeof(workBuffer))!= FR_OK)
+    {
+      printf("Create an FAT/exFAT volume failed.\r\n");
+    }
+    else
+    {
+      printf("Create an FAT/exFAT volume ok.\r\n");
+    }
+    break;
+  case 2:/* mount fs */
+    if(f_mount(&SRAMDISKFatFS, (TCHAR const*)SRAMDISKPath, 0) == FR_OK)
+    {
+      printf("mount FAT/exFAT ok.\r\n");
+    }
+    else
+    {
+      printf("mount FAT/exFAT ok.\r\n");
+    }
+    break;
+  case 3:
+        /* Create and Open a new text file object with write access */
+    if(f_open(&SRAMDISKFile, argv[2], FA_CREATE_ALWAYS | FA_WRITE) == FR_OK)
+    {
+      /* Write data to the text file */
+      res = f_write(&SRAMDISKFile, wtext, sizeof(wtext), (void *)&byteswritten);
+
+      if((byteswritten > 0) && (res == FR_OK))
+      {
+        /* Close the open text file */
+        f_close(&SRAMDISKFile);
+      }
+    }
+    else
+    {
+      printf("open file failed\r\n");
+    }
+    break;
+  case 4:
+    /* Open the text file object with read access */
+    if(f_open(&SRAMDISKFile, argv[2], FA_READ) == FR_OK)
+    {
+        /* Read data from the text file */
+        res = f_read(&SRAMDISKFile, rtext, sizeof(rtext), (void *)&bytesread);
+
+        if((bytesread > 0) && (res == FR_OK))
+        {
+            /* Close the open text file */
+            f_close(&SRAMDISKFile);
+
+            /* Compare read data with the expected data */
+            printf("%s\r\n",rtext);
+        }
+        else
+        {
+          printf("read failed\r\n");
+        }
+      }
+    break;
+  default:
+    break;
+  }
+  return 0;
+}
+LTSH_FUNCTION_EXPORT(fatfs,"fatfs api test");
+
 /* USER CODE END Application */
